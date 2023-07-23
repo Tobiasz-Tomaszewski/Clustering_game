582f715bbe94d1abb7f0b6b0d8b0d54d22078b5c
Tobiasz Tomaszewski <tpt484@poczta.onet.pl>
2023-07-23 14:19:51 +0200

    Now computer responds to every player click with adding random point to coordinate system.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import tkinter as tk
 import numpy as np
 import menu
 import coordinate_system
+import random
 
 points = np.empty(shape=(1, 2))
-print(points)
-
 
 def on_click(event):
     x, y = event.x, event.y
@@ -13,8 +12,16 @@ def on_click(event):
     scaled_y = -(y - origin_y) / scale
     global points
     points = np.vstack((points, np.array([scaled_x, scaled_y])))
-    canvas.create_oval(x, y, x, y, width=3, fill='black')
+    random_x, random_y = random.randint(0, window_width), random.randint(0, window_height)
+    canvas.create_oval(x, y, x, y, width=5, fill='black')
+    canvas.create_oval(random_x, random_y, random_x, random_y, width=5, outline="red")
+    scaled_random_x, scaled_random_y = (random_x - origin_x) / scale, -(random_y - origin_y) / scale
+    points = np.vstack((points, np.array([scaled_random_x, scaled_random_y])))
+    print(points)
 
+def right_click(event):
+    canvas.delete("all")
+    coordinate_system.draw_coordinate_sys(canvas, origin_x, origin_y, window_width, window_height, scale)
 
 # Create the main window
 root = tk.Tk()
@@ -39,6 +46,7 @@ coordinate_system.draw_coordinate_sys(canvas, origin_x, origin_y, window_width, 
 
 # Bind the click event to the canvas
 canvas.bind("<Button-1>", on_click)
+canvas.bind("<Button-3>", right_click)
 
 # Start the Tkinter main loop
 root.mainloop()
