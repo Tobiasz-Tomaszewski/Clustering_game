a0001e3c7b106bbd1d246d0bddd2c8fb5f213640
Tobiasz Tomaszewski <tpt484@poczta.onet.pl>
2023-07-23 10:30:55 +0200

    Update of the main.py file so now the code creates clickable window that creates clickable coordinate system and records all clicks in numpy array.

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import tkinter as tk
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+points = np.empty(shape=(1, 2))
+print(points)
+
+def on_click(event):
+    # Get the coordinates of the click event
+    x, y = event.x, event.y
+    # Convert the coordinates to the coordinate system scale (e.g., 100 pixels = 1 unit)
+    scaled_x = (x - origin_x) / scale
+    scaled_y = -(y - origin_y) / scale
+    global points
+    points = np.vstack((points, np.array([scaled_x, scaled_y])))
+    print(points)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Create the main window
+root = tk.Tk()
+root.title("Clickable Coordinate System")
 
+# Set the size of the window
+window_width, window_height = 400, 400
+root.geometry(f"{window_width}x{window_height}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Set up the coordinate system parameters
+origin_x, origin_y = window_width // 2, window_height // 2  # Center of the window
+scale = 20  # 1 unit = 20 pixels
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Create a canvas widget to draw on
+canvas = tk.Canvas(root, width=window_width, height=window_height, bg="white")
+canvas.pack()
+
+# Draw x and y axes
+canvas.create_line(0, origin_y, window_width, origin_y, fill="black")  # x-axis
+canvas.create_line(origin_x, 0, origin_x, window_height, fill="black")  # y-axis
+
+for i in range(-180, 200, 20):
+    x = i + 200
+    canvas.create_line(x, 198, x, 202, fill="black")  # Tick mark
+    canvas.create_text(x, 210, text=str(i), fill="black")  # Label
+
+# Draw tick marks and labels on Y-axis
+for i in range(-180, 200, 20):
+    y = 200 - i
+    canvas.create_line(198, y, 202, y, fill="black")  # Tick mark
+    canvas.create_text(210, y, text=str(i), fill="black")  # Label
+
+# Bind the click event to the canvas
+canvas.bind("<Button-1>", on_click)
+
+# Start the Tkinter main loop
+root.mainloop()
