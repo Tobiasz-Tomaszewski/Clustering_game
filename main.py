5775eec35d7eec189bf0db87740e6bc36d62c511
Tobiasz Tomaszewski <tpt484@poczta.onet.pl>
2023-07-26 17:49:42 +0200

    WIP. Creating XMeans model and implementing it into the game.

--- main.py
+++ main.py
@@ -4,33 +4,50 @@ import functions
 import random
 import settings
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+from pyclustering.cluster.xmeans import xmeans
+import tkinter.messagebox
 
 
-class ModelXMeans:
-    def __init__(self, data, nr_cluster_init, renderer):
-        self.data = data
-        self.initial_number_of_clusters = nr_cluster_init
-        self.initial_centers = None
-        self.renderer = renderer
+class ModelInterface:
+    def perform_clustering(self, data):
+        raise NotImplementedError()
+
+    @property
+    def nr_of_clusters(self):
+        raise NotImplementedError()
+
+
+class XMeansModel(ModelInterface):
+    def __init__(self):
         self.clusters = None
 
-    def initialize_centers(self):
-        self.initial_centers = kmeans_plusplus_initializer(self.data, self.initial_number_of_clusters).initialize()
+    def perform_clustering(self, data):
+        initial_number_of_clusters = 1
+        initial_centers = kmeans_plusplus_initializer(data, initial_number_of_clusters).initialize()
+        xmeans_instance = xmeans(data, initial_centers, 40)
+        xmeans_instance.process()
+        self.clusters = xmeans_instance.get_clusters()
 
-    def perform_algorithm(self):
-        pass
+    @property
+    def nr_of_clusters(self):
+        return len(self.clusters)
 
-    def get_clusters(self):
-        pass
-        l = []
-        for c in self.clusters:
-            l.append([])
-            for i in c:
-                l[-1].append(self.data[i])
-        return l
 
-    def draw_clusters(self):
-        pass
+class PointCounter:
+    def __init__(self, model, goal_nr):
+        self.model = model
+        self.goal_nr = goal_nr
+
+    def count_score(self, data):
+        """The game can only end with two results. We either win or loose and
+        there is no natural way of counting the score of the player. This method check weather the number of clusters
+        that model has calculated is the same as the goal. If the player wins the game this method will return True,
+        and False if the player looses."""
+        self.model.perform_clustering(data)
+        nr_of_clusters = self.model.nr_of_clusters
+        if nr_of_clusters == self.goal_nr:
+            return True
+        return False
 
 
 class ModelDBSCAN:
@@ -99,19 +116,37 @@ class Game:
         self.renderer = renderer
         self.nr_of_turns = 0
         self.clustering_algorithm = "xmeans"
-        self.end_game = False
+        self.ended = False
+        self.goal_nr = 2
+        self.score = None
 
     def start_game(self):
         self.renderer.draw_coordinate_system()
 
     def player_turn(self, x, y):
+        if self.ended:
+            return
+
         self.add_point(x, y)
         self.renderer.draw_point(x, y, color="black")
         randomX, randomY = random.randint(
             0, settings.window_width), random.randint(0, settings.window_height)
         self.add_point(randomX, randomY)
         self.renderer.draw_point(randomX, randomY, color="red")
+
         self.nr_of_turns += 1
+        if self.nr_of_turns > 20:
+            self.end_game()
+
+    def end_game(self):
+        self.ended = True
+        model = XMeansModel()
+        point_counter = PointCounter(model, self.goal_nr)
+        self.score = point_counter.count_score(self.points)
+        if self.score:
+            tkinter.messagebox.showinfo('Score', 'You have won')
+        else:
+            tkinter.messagebox.showinfo('Score', 'You have lost')
 
     def add_point(self, x, y):
         scaled_x = (x - settings.origin_x) / settings.scale
@@ -122,14 +157,12 @@ class Game:
         self.points = np.empty(shape=(1, 2))
         self.renderer.clear()
         self.renderer.draw_coordinate_system()
+        self.ended = False
+        self.score = None
+        self.nr_of_turns = 0
 
     def change_model(self, model):
         self.clustering_algorithm = model
-
-    def update_state(self):
-        if self.nr_of_turns == 20:
-            self.end_game = True
-        return self.end_game
 
 
 def main():
